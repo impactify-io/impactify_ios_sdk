645e14567180e6ab664d47129ff41e00ec60cdd6
Filipe Neves <filipe.neves@impactify.io>
2023-04-03 11:12:55 +0200

    0.2.0

---
+++ Impactify.framework/Headers/Impactify.h
@@ -0,0 +1,18 @@
+//
+//  Impactify.h
+//  Impactify
+//
+//  Created by Filipe Neves on 03/04/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Impactify.
+FOUNDATION_EXPORT double ImpactifyVersionNumber;
+
+//! Project version string for Impactify.
+FOUNDATION_EXPORT const unsigned char ImpactifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Impactify/PublicHeader.h>
+
+
